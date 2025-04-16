9366d6a1785472a41121c1db3c9d2ba8c9237b1e
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-04-16 15:53:19 -0700

    remove actual query (to not overuse api key)

--- api/search.js
+++ api/search.js
@@ -5,13 +5,6 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "Missing query" });
   }
 
-  const apiKey = process.env.SERP_API_KEY;
-    console.log("apiKey", apiKey);
-  const url = `https://serpapi.com/search.json?q=${encodeURIComponent(query)}&engine=google&num=10&api_key=${apiKey}`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-
   res.status(200).json(data);
 }
 
