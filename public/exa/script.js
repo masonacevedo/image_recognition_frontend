8c5f003a7e23a8de987d70340454384ae540ff4b
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-04-16 16:10:13 -0700

    log data in script.js

--- public/exa/script.js
+++ public/exa/script.js
@@ -4,5 +4,8 @@ async function doSearches() {
     let query = document.getElementById('query-input-box').value;
     const response = await fetch(`/api/search?q=${encodeURIComponent(query)}`);
     console.log("in doSearches");
-    console.log(response);
+    const data = await response.json();
+    console.log("data:", data);
+
+    
 }
