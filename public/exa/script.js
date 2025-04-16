ab0290d32b9d4958293bd0253836277689aeec4c
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-04-16 15:50:50 -0700

    add vercel proxy server (to use SERPAPI token w/out exposing API key)

--- public/exa/script.js
+++ public/exa/script.js
@@ -1,6 +1,8 @@
 async function doSearches() {
     console.log("searches have been completed!");
-    const duckduckgo_url = "https://duckduckgo.com/?q=";
-    const url = duckduckgo_url + encodeURIComponent("mason acevedo");
-    window.open(url, "_blank");
+    
+    let query = document.getElementById('query-input-box').value;
+    const response = await fetch(`/api/search?q=${encodeURIComponent(query)}`);
+    console.log("in doSearches");
+    console.log(response);
 }
