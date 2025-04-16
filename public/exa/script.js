24743aefcff76f3639a2467e77c88886c6597197
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-04-16 13:38:00 -0700

    simple duckduckgo query

--- public/exa/script.js
+++ public/exa/script.js
@@ -1,3 +1,6 @@
 async function doSearches() {
     console.log("searches have been completed!");
+    const duckduckgo_url = "https://duckduckgo.com/?q=";
+    const url = duckduckgo_url + encodeURIComponent("mason acevedo");
+    window.open(url, "_blank");
 }
