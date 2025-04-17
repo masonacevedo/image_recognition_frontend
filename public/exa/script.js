c99f65434d2cddf525d51d4db8ea6e90456a2d30
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-04-17 13:20:38 -0700

    change "search.js" to "google_search.js" for more clear naming

--- public/exa/script.js
+++ public/exa/script.js
@@ -2,7 +2,7 @@ async function doSearches() {
     console.log("searches have been completed!");
     
     let query = document.getElementById('query-input-box').value;
-    const response = await fetch(`/api/search?q=${encodeURIComponent(query)}`);
+    const response = await fetch(`/api/google_search?q=${encodeURIComponent(query)}`);
     console.log("in doSearches");
     const data = await response.json();
     console.log("data:", data);
